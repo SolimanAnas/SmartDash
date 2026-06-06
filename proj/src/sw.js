8e92a3aca51da0943b1df4e8a9be885dceb793c1
Soliman Anas <Solimananas2012@gmail.com>
2026-06-06 22:32:55 +0400

    Fix: format sw.js with prettier

--- proj/src/sw.js
+++ proj/src/sw.js
@@ -30,9 +30,7 @@ self.addEventListener('activate', (event) => {
     (async () => {
       const keys = await caches.keys();
       await Promise.all(
-        keys
-          .filter((k) => k !== CACHE && k !== CDN_CACHE)
-          .map((k) => caches.delete(k)),
+        keys.filter((k) => k !== CACHE && k !== CDN_CACHE).map((k) => caches.delete(k)),
       );
       await self.clients.claim();
     })(),
